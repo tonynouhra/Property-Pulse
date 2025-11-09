2dd8bff6ba7ef3c89fbcdaae5d42b47827f3eeeb
tonynouhra <78659244+tonynouhra@users.noreply.github.com>
2025-11-09 17:04:51 +0200

    feat(properties): enhance property details page with new components
    Added `PropertyDetails` and `PropertyHeaderImage` components to improve property page layout and functionality. Integrated features like contact form, interactive sidebar, and improved styling for better user experience. Introduced loading and error states for robust handling.
    
    BREAKING CHANGE: Custom grid classes (`grid-cols-70/30`) and new theme variables added to `globals.css`, requiring updates for consistent styling across the app.

--- app/properties/[id]/page.jsx
+++ app/properties/[id]/page.jsx
@@ -45,7 +45,12 @@
 'use client'; // Required in App Router for hooks because it works on client side
 import {useEffect, useState} from "react"; // useState is used to store the property data, useEffect is used to fetch the property data when the component is mounted
 import {useParams} from 'next/navigation'; // useParams is used to get the id from the url
-import {fetchProperty} from '@/utils/requests'; // import fetchProperty from utils
+import {fetchProperty} from '@/utils/requests';
+import LoadingPage from "@/app/loading"; // import fetchProperty from utils
+import PropertyHeaderImage from "@/components/PropertyHeaderImage";
+import Link from "next/link";
+import PropertyDetails from "@/components/PropertyDetails";
+import {FaArrowLeft} from 'react-icons/fa'; // import icons
 
 
 const PropertyPage = () => {
@@ -69,22 +74,132 @@ const PropertyPage = () => {
             }
         };
 
-        if (property === null){
+        if (property === null) {
             fetchPropertyData(); // call the fetch function
         }
 
         fetchPropertyData(); // call the fetch function
     }, [id]); // dependency array - refetch if id changes
 
-    if (loading) return <div>Loading...</div>;
+    if (loading) return (<LoadingPage/>);
     if (error) return <div>Error: {error}</div>;
-    if (!property) return <div>Property not found</div>;
+    if (!property && !loading) return <h1 className='text-center text-2xl font-bold'>Property not found</h1>;
 
     return (
-        <div>
-            <h1>{property.name}</h1>
-            {/* Add more property details here */}
-        </div>
+        <>
+            {!loading && property && (<>
+                    <PropertyHeaderImage image={property.images[0]}/>
+                    <section>
+                        <div className="container m-auto py-6 px-6">
+                            <Link
+                                href="/properties"
+                                className="text-blue-500 hover:text-blue-600 flex items-center"
+                            >
+                                <FaArrowLeft className=" mr-2"></FaArrowLeft> Back to Properties
+                            </Link>
+                        </div>
+                    </section>
+
+                    <section className="bg-blue-50">
+                        <div className="container m-auto py-10 px-6">
+                            <div
+                                className="grid grid-cols-1 md:grid-cols-70/30 w-full gap-6"> {/*<!-- custom class -->*/}
+
+                                {/*<!-- Main Content -->*/}
+                                <PropertyDetails property={property}/>
+
+                                {/*<!-- Sidebar -->*/}
+                                <aside className="space-y-4">
+                                    <button
+                                        className="bg-blue-500 hover:bg-blue-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+                                    >
+                                        <i className="fas fa-bookmark mr-2"></i> Bookmark Property
+                                    </button>
+                                    <button
+                                        className="bg-orange-500 hover:bg-orange-600 text-white font-bold w-full py-2 px-4 rounded-full flex items-center justify-center"
+                                    >
+                                        <i className="fas fa-share mr-2"></i> Share Property
+                                    </button>
+
+                                    {/*<!-- Contact Form -->*/}
+                                    <div className="bg-white p-6 rounded-lg shadow-md">
+                                        <h3 className="text-xl font-bold mb-6">Contact Property Manager</h3>
+                                        <form>
+                                            <div className='mb-4'>
+                                                <label
+                                                    className='block text-gray-700 text-sm font-bold mb-2'
+                                                    htmlFor='name'
+                                                >
+                                                    Name:
+                                                </label>
+                                                <input
+                                                    className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                                                    id='name'
+                                                    type='text'
+                                                    placeholder='Enter your name'
+                                                    required
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <label
+                                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                                    htmlFor="email"
+                                                >
+                                                    Email:
+                                                </label>
+                                                <input
+                                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
+                                                    id="email"
+                                                    type="email"
+                                                    placeholder="Enter your email"
+                                                    required
+                                                />
+                                            </div>
+                                            <div className='mb-4'>
+                                                <label
+                                                    className='block text-gray-700 text-sm font-bold mb-2'
+                                                    htmlFor='phone'
+                                                >
+                                                    Phone:
+                                                </label>
+                                                <input
+                                                    className='shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline'
+                                                    id='phone'
+                                                    type='text'
+                                                    placeholder='Enter your phone number'
+                                                />
+                                            </div>
+                                            <div className="mb-4">
+                                                <label
+                                                    className="block text-gray-700 text-sm font-bold mb-2"
+                                                    htmlFor="message"
+                                                >
+                                                    Message:
+                                                </label>
+                                                <textarea
+                                                    className="shadow appearance-none border rounded w-full py-2 px-3 text-gray-700 h-44 focus:outline-none focus:shadow-outline"
+                                                    id="message"
+                                                    placeholder="Enter your message"
+                                                ></textarea>
+                                            </div>
+                                            <div>
+                                                <button
+                                                    className="bg-blue-500 hover:bg-blue-600 text-white font-bold py-2 px-4 rounded-full w-full focus:outline-none focus:shadow-outline flex items-center justify-center"
+                                                    type="submit"
+                                                >
+                                                    <i className="fas fa-paper-plane mr-2"></i> Send Message
+                                                </button>
+                                            </div>
+                                        </form>
+                                    </div>
+                                </aside>
+                            </div>
+                        </div>
+                    </section>
+                </>
+            )}
+
+        </>
     );
 };
 
---
+++ components/PropertyHeaderImage.jsx
@@ -0,0 +1,27 @@
+import Image from "next/image";
+
+
+const PropertyHeaderImage = ({image}) => {
+    return(
+        <section>
+            <div className="container-xl m-auto">
+                <div className="grid grid-cols-1">
+                    <Image
+                        src={`/images/properties/${image}`}
+                        alt=""
+                        className="object-cover h-[400px] w-full"
+                        width={1800}
+                        height={1200}
+                        sizes='100vw'
+                        priority={true}
+                    />
+                </div>
+            </div>
+        </section>
+
+
+    )
+}
+
+
+export default PropertyHeaderImage;
